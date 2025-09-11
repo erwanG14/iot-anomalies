39f7dbff1785e54cd950ab0a9ebe9207de0a5307
erwanG14 <erwan.germain14@gmail.com>
2025-09-11 12:46:38 +0200

    API springboot

---
+++ backend/anomalies-api/src/main/java/com/example/anomalies_api/AnomaliesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.anomalies_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomaliesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomaliesApiApplication.class, args);
+	}
+
+}
---
+++ backend/anomalies-api/src/test/java/com/example/anomalies_api/AnomaliesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anomalies_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnomaliesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
